0d6fcf921f4698d04957dfd0c0db404293e20c46
Franco <ana.flavia.s.franco@accenture.com>
2019-08-14 16:31:33 -0300

    Improving EntryFormComponent

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -5,6 +5,10 @@ import { ActivatedRoute, Router } from "@angular/router";
 import { Entry } from "../shared/entry.model";
 import { EntryService } from "../shared/entry.service";
 
+import { Category } from "../../categories/shared/category.model";
+import { CategoryService } from "../../categories/shared/category.service";
+
+
 import { switchMap } from "rxjs/operators"; //manipular a rota
 
 import toastr from "toastr";
@@ -22,18 +26,44 @@ export class EntryFormComponent implements OnInit {
   serverErrorMessages: string[] = null; //mensagens com erro vindas do servidor
   submittingForm: boolean = false; //desabilitar botao enviar p/ nao enviar a mesma coisa varias vezes
   entry: Entry = new Entry();
+  categories: Array<Category>;
+
+  /*imaskConfig = {
+    mask: Number,
+    scale: 2,
+    thousandsSeparator: '',
+    padFractionalZeros: true,
+    normalizeZeros: true,
+    radix: ',',
+  } */
+
+  ptBR = {
+    firstDayOfWeek: 0,
+    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+    dayNamesMin: ['Do', 'Se', "Te", 'Qu', 'Qu', 'Se', 'Sa'],
+    monthNames: [
+      'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto',
+      'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    today: 'Hoje',
+    clear: 'Limpar'
+  }
 
   constructor(
     private entryService: EntryService,
     private route: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit() {
     this.setCurrentAction();
     this.buildEntryForm();
     this.loadEntry();
+    this.loadCategories();
   }
 
   ngAfterContentChecked(){
@@ -48,6 +78,17 @@ export class EntryFormComponent implements OnInit {
       //currentAction == "edit"
       this.updateEntry();
     }
+  }
+
+  get typeOptions(): Array<any>{
+    return Object.entries(Entry.types).map(
+      ([value, text]) => {
+        return {
+          text: text,
+          value: value
+        }
+      }
+    )
   }
 
 
@@ -66,10 +107,10 @@ export class EntryFormComponent implements OnInit {
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null],
-      type: [null, [Validators.required]],
+      type: ["expense", [Validators.required]],
       amount: [null,[Validators.required]],
       date: [null, [Validators.required]],
-      paid: [null, [Validators.required]],
+      paid: [true, [Validators.required]],
       categoryId: [null, [Validators.required]]
     });
   }
@@ -88,6 +129,12 @@ export class EntryFormComponent implements OnInit {
       )
     }
   }
+
+private loadCategories(){
+  this.categoryService.getAll().subscribe(
+    categories => this.categories = categories
+  );
+}
 
   private setPageTitle (){
     if(this.currentAction == 'new'){
